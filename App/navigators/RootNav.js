29e03adae3b972ff25bf74e9acd1cf7422962ba8
George Kando <gkando@gmail.com>
2019-10-22 14:52:25 -0500

    switching pr from master to develop

--- App/navigators/RootNav.js
+++ App/navigators/RootNav.js
@@ -4,13 +4,13 @@ import AppStackNav from "./AppStackNav";
 
 // THIS IS THE ROOT NAVIGATOR IT CONNECTS THE NAVIGATION COMPONENTS SD 10/21/2019
 const RootNav = createStackNavigator({
-  // Guest: {
-  //SCREEN / COMPONENT TO BE RENDERED SD 10/21/2019
-  //   screen: AppStackNav,
-  //   navigationOptions: {
-  //     header: null,
-  //   },
-  // },
+  Guest: {
+  // SCREEN / COMPONENT TO BE RENDERED SD 10/21/2019
+    screen: AppStackNav,
+    navigationOptions: {
+      header: null,
+    },
+  },
   Dashboard: {
     screen: Dashboard
   }
