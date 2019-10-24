26c5d4c0f5757d9cbc530b64b9d6cb403eee7e97
dannyvidal <dannyjvidal94@gmail.com>
2019-10-24 17:36:15 -0400

    added form validation

--- screens/Login/Signup.js
+++ screens/Login/Signup.js
@@ -1,159 +1,187 @@
 import React, { useState, useRef } from "react";
 // import { useDispatch } from 'react-redux'
 import { Formik } from 'formik';
-import { 
-          StyleSheet,
-          Text,
-          View,
-          TextInput,
-          KeyboardAvoidingView,
-          TouchableOpacity
-        } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity
+} from "react-native";
 import Swiper from 'react-native-swiper';
 // import { doSignup} from "../store/actions/authActions";
 import * as Yup from 'yup';
 import Camera from '../../components/camera/Camera';
+import { isUpdateExpression } from "@babel/types";
 //To-Do
 //  Input validation -- functions built out just need to implement
 //  Data sent and received by server - need to build after signup process
 //  Add all required fields (role, picture, authId??)
 //  Formatting and styling
 const Signup = (props) => {
-  // Need to clean up a lot of this code - was plowing ahead towards a solution & mvp.
-  const [user, setUser] = useState({});
-  const [disabled, setDisabled] = useState(false);
-  const [err, setErr] = useState();
+
   const [current, setCurrent] = useState(0);
-  // const dispatch = useDispatch();
-  const onInputChange = (name, text) => {
-    const updatedUser = { ...user, [name]: text };
-    setUser(updatedUser)
-    console.log(user)
-    // setUser({ ...user, [name]: text})
-  };
-  const handleSubmit = () => {
-    console.log('user', user)
-    const { username, email, phone } = user
-    const password = 123456 //temp password for testing
-    const newUser = `mutation { signUp( username: "${username}", password: "${password}", email: "${email}", phone: "${phone}" ) { token user {id} } }`
-    // dispatch(doSignup(newUser))
-  };
+  var [formValues, setFormValues] = useState({
+    username:"",
+    email:"",
+    phone:""
+  })
+  const [disabled, setDisabled] = useState(false);
   const swipeRef = useRef();
+  const phoneRegExp = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
   const pages = [
     {
+      name: "email",
       slideTitle: "Welcome to Netgiver!",
       text: "We just need to get some info before you get started",
       text2: "Please enter your email:",
-      name: "email",
       keyboard: "email-address",
-      placeholder: "Email"
+      placeholder: "Email",
+      schema: {
+        email: Yup.string().email("Must be a valid email.").required("Email is required."),
+      }
     },
     {
+      name: "username",
       slideTitle: "Welcome to Netgiver!",
       text: "We just need to get some info before you get started",
       text2: "Please enter your username:",
-      name: "username",
-      placeholder: "username"
+      placeholder: "username",
+      schema: {
+        username: Yup.string().min(2).max(50).required('Username is required.'),
+      }
     },
     {
+      name: "phone",
       slideTitle: "Welcome to Netgiver!",
       text: "",
       text2: "Please enter your phone number:",
-      name: "phone",
       placeholder: "Phone Number",
-      keyboard: "phone-pad"
+      keyboard: "phone-pad",
+      schema: {
+        phone: Yup.string().matches(phoneRegExp, 'Phone number is not valid').required("Phone number is required."),
+      }
     },
     {
-      type: "photo",
-    },
+      type:"photo"
+    }
   ]
-  const SignupSchema = Yup.object().shape({
-    firstName: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
-    lastName: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
-    email: Yup.string()
-      .email('Invalid email')
-      .required('Required'),
-  });
-  const LastSlide = () => {
-    return(
-    <View style={styles.slide4}>
-    <Text style={styles.title}>
-      Phone
-    </Text>
-    <View style={styles.inputContainer}>
-      <Text style={styles.text}>
-        Please select a profile photo:
-      </Text>
-      <TouchableOpacity style={styles.buttonStyle} onPress={() => props.navigation.navigate('Camera', {from:'Signup'})}>
-      <Text style={styles.buttonText}>Use the Camera</Text>
-    </TouchableOpacity>
-      <TouchableOpacity style={styles.buttonStyle} onPress={() => handleSubmit()}>
-        <Text style={styles.buttonText}>Submit</Text>
-      </TouchableOpacity>
-      </View>
-  </View>
-    )
-  }
-  const handleClick = () => {
+
+
+  function handleSubmit(){
+    var { username, email, phone } = formValues;
+    var password = "12512"//test password;
+    var newUser = `mutation { signUp( username: "${username}", password: "${password}", email: "${email}", phone: "${phone}" ) { token user {id} } }`
+    console.log("final values",formValues)// TODO : do something with this <---
+  };
+  const handleNext = (values) => {
+    //this function will set page to the next page when schema passes and change state
+    setFormValues({...formValues, ...values});
     swipeRef.current.scrollBy(1, true)
     setCurrent(current + 1)
   }
-  return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <Formik
-        onSubmit={values => console.log(values)}
-        validationSchema={SignupSchema}
-        initialValues={{ email: '', username: '', phone: '' }}
-        >
-        <Swiper 
-          ref={swipeRef}
-          style={styles.wrapper}
-          showsButtons={false}
-          disableNextButton={disabled}
-          loop={false}
-          buttonWrapperStyle={{position: "relative", marginVertical: 80, paddingHorizontal: 0}}
-        >
-          {pages.map((input, index) =>  {
-            if (input.type === 'photo'){
-              return <LastSlide />
-            } else
-            return (
-              <View style={styles['slide' + ++index]} key={'slide' + input.id}>
-                <Text style={styles.title}> {input.slideTitle} </Text>
-                <Text style={styles.text}> {input.text} </Text>
-                <View style={styles.inputContainer}>
-                  <Text style={styles.text}> {input.text2} </Text>
-                  <TextInput
-                    key={input.name + input.id}
-                    name={input.name}
-                    value={user[input.name]}
-                    keyboardType={input.keyboard}
-                    onChangeText={(text) => onInputChange(input.name, text)}
-                    placeholder={input.placeholder}
-                    style={styles.input}
-                  />
-                  <TouchableOpacity style={styles.buttonStyle} onPress={() => handleClick()}>
-                    <Text style={styles.buttonText}>Next</Text>
-                  </TouchableOpacity>
-                </View>
+  function Form(slide) {
+    var schema = Yup.object().shape(this.schema);
+    return <Formik
+      onSubmit={(values, formikBag) => {
+        formikBag.setSubmitting(false);
+        handleNext(values, formikBag)
+      }}
+      validationSchema={schema}
+      initialValues={{
+        [this.name]:""
+      }}
+      render={props => {
+        return slide.call(this, props);
+      }}
+    />
+  }
+  function Slide(props) {
+
+    return <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <Swiper
+        ref={swipeRef}
+        style={styles.wrapper}
+        showsButtons={false}
+        disableNextButton={disabled}
+        loop={false}
+        buttonWrapperStyle={{ position: "relative", marginVertical: 80, paddingHorizontal: 0 }}
+      >{
+          (this.type === 'photo') ? <LastSlide {...props} />
+            :
+            <View style={styles.slide}>
+              <Text style={styles.title}> {this.slideTitle} </Text>
+
+              <Text style={styles.text}> {this.text} </Text>
+              <View style={styles.inputContainer}>
+                <Text style={styles.text}> {this.text2} </Text>
+                <TextInput
+                  key={this.name + this.id}
+                  name={this.name}
+                  value={props.values[this.name]}
+                  keyboardType={this.keyboard}
+                  onChangeText={props.handleChange(this.name)}
+                  placeholder={this.placeholder}
+                  style={styles.input}
+                />
+                <Text >{
+                  props.touched[this.name] &&  props.errors[this.name]
+                  ? props.errors[this.name] : null
+                }</Text>
+                <TouchableOpacity style={styles.buttonStyle} onPress={() => {
+                  props.handleSubmit(props.values);
+                }}>
+                  <Text style={styles.buttonText}>Next</Text>
+                </TouchableOpacity>
               </View>
-            )})}
-          
-    </Swiper>
-    </Formik>
-  </KeyboardAvoidingView>
-  );
+            </View>}
+
+      </Swiper>
+    </KeyboardAvoidingView>
+  }
+  function LastSlide(props) {
+    return (
+      <View style={styles.slide}>
+        <Text style={styles.title}>
+          Phone
+    </Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.text}>
+            Please select a profile photo:
+      </Text>
+          <TouchableOpacity style={styles.buttonStyle} onPress={() => props.navigation.navigate('Camera', { from: 'Signup' })}>
+            <Text style={styles.buttonText}>Use the Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyle} onPress={() => {
+             handleSubmit()
+          }}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+  /*this switch statement will determine the this value for each form function call
+    whats great about this is that now all you have to do is modify this switch case statement 
+    and add to the variable pages if you want to add a screen
+  */
+  switch (current) {
+    case 0:
+      return Form.call(pages[0], Slide);
+    case 1:
+      return Form.call(pages[1], Slide);
+    case 2:
+      return Form.call(pages[2], Slide);
+    case 3:
+      return Form.call(pages[3], Slide);
+  }
+
 }
 const styles = StyleSheet.create({
   wrapper: {
   },
-  container: {flex: 1},
+  container: { flex: 1 },
   inputContainer: {
     marginTop: 50,
     width: '100%',
@@ -176,38 +204,26 @@ const styles = StyleSheet.create({
     padding: 10,
     color: 'white'
   },
-buttonStyle: {
-  padding:2,
-  backgroundColor: '#006E13',
-  alignItems: 'center',
-  borderRadius:4,
-  width: '100%',
+  buttonStyle: {
+    padding: 2,
+    backgroundColor: '#006E13',
+    alignItems: 'center',
+    borderRadius: 4,
+    width: '100%',
   },
-btnNext: {
-  color: 'green'
-},
-  slide1: {
+  btnNext: {
+    color: 'green'
+  },
+  slide: {
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
-    
   },
-  slide2: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    
-  },
+
   title: {
     color: '#282424',
     fontSize: 26,
-    
+
     marginTop: '10%',
     textAlign: 'center',
     paddingBottom: 3,
