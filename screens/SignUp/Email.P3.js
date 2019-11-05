7933af233d5e74085c2b57b270b94e0423f97286
skyler <skylerwebdev@gmail.com>
2019-11-05 17:49:26 -0500

    setting up new signUp

--- screens/SignUp/Email.P3.js
+++ screens/SignUp/Email.P3.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {SafeAreaView, Text, TouchableOpacity, TextInput, Image, StyleSheet} from 'react-native'
 
 const Email = () => {
     return (
-        <div>
+        <SafeAreaView>
             
-        </div>
+        </SafeAreaView>
     )
 }
 
