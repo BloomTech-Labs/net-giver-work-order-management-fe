3966db51d1c61fc36453343c3ba30f429a3d338d
Bryant Patton <bryantpatton@gmail.com>
2019-11-07 14:27:18 -0800

    apollo plugin config

--- apollo.config.js
+++ apollo.config.js
@@ -1,21 +1,21 @@
-// module.exports = {
-//   client: {
-//     name: "womapi",
-//     service: "womapi"
-//   }
-// };
-
 module.exports = {
   client: {
-    service: {
-      // name: "womapi",
-      url: "http://localhost:3000/graphql",
-      // optional headers
-      // optional disable SSL validation check
-      skipSSLValidation: true
-    }
+    name: "womapi",
+    service: "womapi"
   }
 };
+
+// module.exports = {
+//   client: {
+//     service: {
+//       // name: "womapi",
+//       url: "http://localhost:3000/graphql",
+//       // optional headers
+//       // optional disable SSL validation check
+//       skipSSLValidation: true
+//     }
+//   }
+// };
 
 // module.exports = {
 //   client: {
