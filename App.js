2244d757136288c5e0f8115214433570e105d4d7
Kizmet <bryantpatton@gmail.com>
2019-11-06 23:55:44 -0800

    wo schema

--- App.js
+++ App.js
@@ -129,8 +129,8 @@ const client = new ApolloClient({
     //   // uri: "http://localhost:3000/graphql"
     //   uri: "https://netgiver-stage.herokuapp.com/graphql"
     // })
-    createUploadLink({ uri: "http://localhost:3000/graphql" })
-    // createUploadLink({ uri: "https://netgiver-stage.herokuapp.com/graphql" })
+    // createUploadLink({ uri: "http://localhost:3000/graphql" })
+    createUploadLink({ uri: "https://netgiver-stage.herokuapp.com/graphql" })
   ]),
   cache
 });
