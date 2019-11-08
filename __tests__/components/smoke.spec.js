b30cd8e58caf31ad30eec9c7d5265a153db75bcc
laura <lauradondiego@gmail.com>
2019-11-08 16:07:19 -0500

    merging

--- __tests__/components/smoke.spec.js
+++ __tests__/components/smoke.spec.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { shallow } from 'enzyme'
+import React from "react";
+import { Text } from "react-native";
+import { shallow } from "enzyme";
 
-describe('Text', () => {
-    it('renders text', () => {
-        const wrapper = shallow(<Text>Hello, world!</Text>)
-        expect(wrapper.text()).toEqual('Hello, world!')
-    })
-})
+describe("Text", () => {
+  it("renders text", () => {
+    const wrapper = shallow(<Text>Hello, world!</Text>);
+    expect(wrapper.text()).toEqual("Hello, world!");
+  });
+});
 
 // 11.8 TESTING ENZYME LD
 
