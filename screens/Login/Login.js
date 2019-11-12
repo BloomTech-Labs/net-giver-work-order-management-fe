ff75713694219257d585fec799646f7f0dab63a9
laura <lauradondiego@gmail.com>
2019-11-12 11:36:41 -0500

    seting up detox

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -1,110 +1,111 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  TouchableOpacity,
-  Alert,
-  Image
-} from "react-native";
-import axios from "axios";
-import { Button } from "native-base";
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    TouchableOpacity,
+    Alert,
+    Image,
+} from "react-native"
+import axios from "axios"
+import { Button } from "native-base"
 
-import { loginStyles, styles } from "../../components/Styles";
-import logo from "../../components/Images/NetGiverLogo.svg";
-import SafeAreaView from "react-native-safe-area-view";
-import { gql } from "apollo-boost";
-import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import { loginStyles, styles } from "../../components/Styles"
+import logo from "../../components/Images/NetGiverLogo.svg"
+import SafeAreaView from "react-native-safe-area-view"
+import { gql } from "apollo-boost"
+import { useApolloClient, useMutation } from "@apollo/react-hooks"
 
 export const SIGN_IN_DEV = gql`
-  mutation signInDev($username: String!) {
-    signInDev(username: $username) {
-      username
+    mutation signInDev($username: String!) {
+        signInDev(username: $username) {
+            username
+        }
     }
-  }
-`;
+`
 
 const Login = props => {
-  const [username, onChangeText] = useState("");
-  const client = useApolloClient();
-  const { navigation } = props;
-  const [signInDev, { loading, error }] = useMutation(SIGN_IN_DEV, {
-    onCompleted({ signInDev }) {
-      const username = signInDev.username;
-      console.log(username);
-      props.navigation.navigate("VerifyLogin", { username: username });
-      // client.writeData({ data: { isLoggedIn: true } });
+    const [username, onChangeText] = useState("")
+    const client = useApolloClient()
+    const { navigation } = props
+    const [signInDev, { loading, error }] = useMutation(SIGN_IN_DEV, {
+        onCompleted({ signInDev }) {
+            const username = signInDev.username
+            console.log(username)
+            props.navigation.navigate("VerifyLogin", { username: username })
+            // client.writeData({ data: { isLoggedIn: true } });
+        },
+    })
+    //SENDS BACK TO LOGIN INCASE OF NO TOKEN 10/24/2019 SD
+    const goBack = () => {
+        props.navigation.navigate("AuthLoading")
     }
-  });
-  //SENDS BACK TO LOGIN INCASE OF NO TOKEN 10/24/2019 SD
-  const goBack = () => {
-    props.navigation.navigate("AuthLoading");
-  };
 
-  if (loading)
+    if (loading)
+        return (
+            <SafeAreaView style={styles.container}>
+                <Text style={loginStyles.header}>Loading</Text>
+            </SafeAreaView>
+        )
+    if (error)
+        return (
+            <SafeAreaView style={styles.container}>
+                <Text style={loginStyles.header} testID="header">
+                    Invalid Username :( {console.log(error)}
+                </Text>
+                <Button onPress={goBack} style={styles.button}>
+                    <Text>Try again!</Text>
+                </Button>
+            </SafeAreaView>
+        )
     return (
-      <SafeAreaView style={styles.container}>
-        <Text style={loginStyles.header}>Loading</Text>
-      </SafeAreaView>
-    );
-  if (error)
-    return (
-      <SafeAreaView style={styles.container}>
-        <Text style={loginStyles.header}>
-          Invalid Username :( {console.log(error)}
-        </Text>
-        <Button onPress={goBack} style={styles.button}>
-          <Text>Try again!</Text>
-        </Button>
-      </SafeAreaView>
-    );
-  return (
-    <SafeAreaView style={styles.container}>
-      <Image
-        style={loginStyles.logo}
-        source={require("../../components/Images/ng.png")}
-      />
-      {/* </View> */}
-      <Text style={loginStyles.header}>Sign In</Text>
-      <Text style={loginStyles.subHeader}>
-        And leave your paperwork behind!
-      </Text>
-      <TextInput
-        style={loginStyles.loginTextInput}
-        placeholder="Username"
-        name="username"
-        value={username}
-        autoCapitalize="none"
-        onChangeText={text => onChangeText(text)}
-        onFocus={() => onChangeText("")}
-      />
-      {/* <View style={loginStyles.signIn}> */}
-      <Button
-        style={[loginStyles.buttons, {marginTop: 20}]}
-        onPress={() =>
-          signInDev({
-            variables: {
-              username: username
-            }
-          })}
-      >
-        <Text style={loginStyles.buttonText}>Sign In</Text>
-      </Button>
+        <SafeAreaView style={styles.container}>
+            <Image
+                style={loginStyles.logo}
+                source={require("../../components/Images/ng.png")}
+            />
+            {/* </View> */}
+            <Text style={loginStyles.header}>Sign In</Text>
+            <Text style={loginStyles.subHeader}>
+                And leave your paperwork behind!
+            </Text>
+            <TextInput
+                style={loginStyles.loginTextInput}
+                placeholder="Username"
+                name="username"
+                value={username}
+                autoCapitalize="none"
+                onChangeText={text => onChangeText(text)}
+                onFocus={() => onChangeText("")}
+            />
+            {/* <View style={loginStyles.signIn}> */}
+            <Button
+                style={[loginStyles.buttons, { marginTop: 20 }]}
+                onPress={() =>
+                    signInDev({
+                        variables: {
+                            username: username,
+                        },
+                    })
+                }
+            >
+                <Text style={loginStyles.buttonText}>Sign In</Text>
+            </Button>
 
-      <Text style={loginStyles.buttonHeader}>Don't Have an Account?</Text>
+            <Text style={loginStyles.buttonHeader}>Don't Have an Account?</Text>
 
-      <Button
-        onPress={() => props.navigation.navigate("P1")}
-        style={[loginStyles.buttons, {marginTop: 20}]}
-      >
-        <Text style={loginStyles.buttonText}>Sign Up</Text>
-      </Button>
-      {/* NEEDS TO LINK TO CONTACT */}
-      <Text style={loginStyles.footerText}>Contact Netgiver Team</Text>
-    </SafeAreaView>
-  );
-};
+            <Button
+                onPress={() => props.navigation.navigate("P1")}
+                style={[loginStyles.buttons, { marginTop: 20 }]}
+            >
+                <Text style={loginStyles.buttonText}>Sign Up</Text>
+            </Button>
+            {/* NEEDS TO LINK TO CONTACT */}
+            <Text style={loginStyles.footerText}>Contact Netgiver Team</Text>
+        </SafeAreaView>
+    )
+}
 
 // LEFT IN STYLESHEET SO THAT IT COULD BE LOOKED AT EASILY FOR REFERENCE 10/25/2019 SD
 
@@ -211,4 +212,4 @@ const Login = props => {
 //   }
 // });
 
-export default Login;
+export default Login
